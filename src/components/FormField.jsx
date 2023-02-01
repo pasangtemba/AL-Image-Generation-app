f551300cb56ff425d93003f01982ec97a171b58b
pasangtemba <pasangtemba.sherpa5@gmail.com>
2023-02-01 12:41:45 +0545

    connected to backend

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const FormField = ({
-  labelName, type,  name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe,
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
 }) => (
   <div>
     <div className="flex items-center gap-2 mb-2">
-      <label
-        htmlFor={name}
-        className="block text-sm font-medium text-gray-900"
-      >
+      <label htmlFor={name} className="block text-sm font-medium text-gray-900">
         {labelName}
       </label>
       {isSurpriseMe && (
